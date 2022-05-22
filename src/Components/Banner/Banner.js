605bc72ca7a0707044d6c5810d6e1f6593e0cd9e
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-22 09:15:06 +0600

    Banner component created

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="banner">
+        <div className="banner-txt-section">
+          <h3 className="banner-txt">Let's paint around you.</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
