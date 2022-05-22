683efb1e0f3978eb2de6c0fe106801ea0888f476
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-22 09:48:17 +0600

    Card component created

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div class="card w-80 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://api.lorem.space/image/shoes?w=400&h=225"
+          alt="Shoes"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>
+          <b>Price: </b>
+          <span>10$</span>
+        </p>
+        <p>
+          <b>Available: </b>
+          <span>10$</span>
+        </p>
+        <p>
+          <b>Minimum oder: </b>
+          <span>10$</span>
+        </p>
+        <p>
+          <b>Short description: </b>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem
+            voluptas consectetur tempore fuga, quidem rem quae nulla deserunt.
+            Sit eaque maiores necessitatibus sed possimus dolores animi
+            voluptates amet? Velit, voluptatum.
+          </span>
+        </p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Order Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../../Components/Banner/Banner";
+import Card from "../../Components/Card/Card";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Card></Card>
     </div>
   );
 };
