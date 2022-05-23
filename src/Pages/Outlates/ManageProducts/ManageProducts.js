94ef4014c34d12834922df7fc2bd57b3c31210d7
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-23 18:58:51 +0600

    Manage products page created

--- src/Pages/Outlates/ManageProducts/ManageProducts.js
+++ src/Pages/Outlates/ManageProducts/ManageProducts.js
@@ -3,7 +3,39 @@ import React from "react";
 const ManageProducts = () => {
   return (
     <div>
-      <h1>Manage Products</h1>
+      <h2 className=" text-3xl my-5 ml-8">Manage products</h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>Product name</th>
+              <th>Available stock</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Brush</th>
+              <td>20$</td>
+              <td>200 units</td>
+              <td>Paid</td>
+            </tr>
+            <tr>
+              <th>Brush</th>
+              <td>20$</td>
+              <td>200 units</td>
+              <td>Paid</td>
+            </tr>
+            <tr>
+              <th>Brush</th>
+              <td>20$</td>
+              <td>200 units</td>
+              <td>Paid</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
