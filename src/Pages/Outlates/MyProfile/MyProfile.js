747b36f3ec8938fe410dc3bbe038cd537f8a8503
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-23 11:16:30 +0600

    My profile page created

--- src/Pages/Outlates/MyProfile/MyProfile.js
+++ src/Pages/Outlates/MyProfile/MyProfile.js
@@ -3,7 +3,37 @@ import React from "react";
 const MyProfile = () => {
   return (
     <div>
-      <h1>My profile</h1>
+      <h2 className="text-3xl my-5 ml-8">My profile</h2>
+      <div className="flex justify-center items-center">
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="avatar flex justify-center">
+            <div class="w-24 rounded-full">
+              <img src="https://api.lorem.space/image/face?hash=92310" />
+            </div>
+          </div>
+          <div class="card-body">
+            <h2 class="card-title">Rakibul Hasan</h2>
+            <p>
+              <b>Email: </b>rakib@hasan.com
+            </p>
+            <p>
+              <b>Location: </b>rakib@hasan.com
+            </p>
+            <p>
+              <b>Phone number: </b>rakib@hasan.com
+            </p>
+            <p>
+              <b>Email: </b>rakib@hasan.com
+            </p>
+            <p>
+              <b>Current role: </b>rakib@hasan.com
+            </p>
+            <div class="card-actions">
+              <button class="btn btn-primary">Update Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
